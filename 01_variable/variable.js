743fc1812f987e6b5db6a72fac26e87df8df4263
Shyam Pareek <35099798+shyam4ssp@users.noreply.github.com>
2024-01-04 06:09:07 +0000

    datatype

---
+++ 01_variable/variable.js
@@ -0,0 +1,7 @@
+const name = 'Shyam';
+var village = 'Bukansar';
+var age = 26;
+let profile = 'Magento Frontend Developer';
+
+
+console.table([name, village, age, profile]);
---
+++ 02_ecma/ecma.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all code as newer js version
+
+let name = "Shyam"; // string
+let age = 26; // number, bigInt
+let isLoggedin = false; // boolean
+let state = null; // standalone value, null is a object
+
+// undefined - undefined variable
+// symbol - unique
+
+// objects
